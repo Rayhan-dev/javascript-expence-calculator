219aeef2c2182c657a32c8d869d770aa2e8cc165
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-02-17 05:02:37 +0600

    error handle 1 done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
-const incomeAmmount = gettingElement('income_field');
-const foodCost = gettingElement('food_field');
-const rentCost = gettingElement('rent_field');
-const clothesCost = gettingElement('clothes_field');
-const totalExpenses = gettingElement('total_expenses');
-const balanceAfterExpenses = gettingElement('balance');
-
-function gettingElement(name){
-    let element = document.getElementById(name);
+// getting element by ID 
+function gettingElement(idName){
+    let element = document.getElementById(idName);
     return element;
 }
 
-function calculateExpences(){
-    totalExpenses.innerText= parseInt(foodCost.value)+parseInt(rentCost.value)+parseInt(clothesCost.value);
-    balanceAfterExpenses.innerText=parseInt(incomeAmmount.value)-parseInt(totalExpenses.innerText);
+function calculatingExpenses(){
+    // getting values 
+    const incomeAmmount = gettingElement('income_field').value;
+    const foodCost = gettingElement('food_field').value;
+    const rentCost = gettingElement('rent_field').value;
+    const clothesCost = gettingElement('clothes_field').value;
+    // getting innerTexts 
+    const totalExpenses = gettingElement('total_expenses');
+    const balanceAfterExpenses = gettingElement('balance');
+    // handling error with conditions 
+    if(isNaN(parseInt(incomeAmmount)) || parseInt(incomeAmmount)<0 ){
+        alert("please input a positive number in income field");
+    }else if(isNaN(parseInt(foodCost)) || parseInt(foodCost)<0 ){
+        alert("please input a positive number in food field");
+    }else if(isNaN(parseInt(rentCost)) || parseInt(rentCost)<0 ){
+        alert("please input a positive number in rent field");
+    }else if(isNaN(parseInt(clothesCost)) || parseInt(clothesCost)<0 ){
+        alert("please input a positive number in clothes field");
+    }else{
+        totalExpenses.innerText= parseInt(foodCost)+parseInt(rentCost)+parseInt(clothesCost);
+        balanceAfterExpenses.innerText=parseInt(incomeAmmount)-parseInt(totalExpenses.innerText);
+    }
 }
+
