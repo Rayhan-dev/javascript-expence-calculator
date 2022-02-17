029a3e275189d84601caa714c2ab9ddf8ed6a537
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-02-17 15:30:42 +0600

    js problems solved

--- js/script.js
+++ js/script.js
@@ -1,50 +1,75 @@
-// function for getting elements 
-function gettingElement(idName){
-    let element = document.getElementById(idName);
-    return element;
+// function for getting values as numbers 
+function getParsedValue(idName){
+    var persedValue =parseInt(document.getElementById(idName).value);
+    return persedValue
 }
 // getting elements 
-    let incomeAmmount = gettingElement('income_field'); 
-    let foodCost = gettingElement('food_field'); 
-    let rentCost = gettingElement('rent_field');
-    let clothesCost = gettingElement('clothes_field');
-    let totalExpenses = gettingElement('total_expenses'); //total expenses of food,rent,clothes
-    let balanceAfterExpenses = gettingElement('balance'); 
- // function for calculation expences
+function getElement(idName){
+    var element =document.getElementById(idName);
+    return element;
+}
 function calculatingExpenses(){
-   //total expenses of food,rent,clothes
-    let totalCost = parseInt(foodCost.value)+parseInt(rentCost.value)+parseInt(clothesCost.value);
-     // handling error with conditions 
-    if(typeof(parseInt(incomeAmmount.value)) != 'number' || parseInt(incomeAmmount.value)<0 ){
+    // getting values and elements 
+    var incomeAmmount = getParsedValue("income_field"); 
+    var foodCost = getParsedValue("food_field"); 
+    var rentCost = getParsedValue("rent_field");
+    var clothesCost = getParsedValue("clothes_field");
+    var totalExpenses = getElement("total_expenses");
+    var balanceAfterExpenses = getElement("balance");
+    // total cost 
+    var totalCost = foodCost + rentCost + clothesCost;
+
+    //  handling error with conditions 
+    if(isNaN(incomeAmmount) || incomeAmmount < 0 ){
         alert("please input a positive number in 'income field'");
-    }else if(typeof(parseInt(foodCost.value)) != 'number' || parseInt(foodCost.value)<0 ){
+    }else if(isNaN(foodCost) || foodCost < 0 ){
         alert("please input a positive number in 'food field'");
-    }else if(typeof(parseInt(rentCost.value)) != 'number'|| parseInt(rentCost.value)<0 ){
+    }else if(isNaN(rentCost) || rentCost < 0 ){
         alert("please input a positive number in 'rent field'");
-    }else if(typeof(parseInt(clothesCost.value)) != 'number' || parseInt(clothesCost.value)<0 ){
+    }else if(isNaN(clothesCost) || clothesCost < 0 ){
         alert("please input a positive number in 'clothes field'");
-        return ;
-    }else if((parseInt(incomeAmmount.value))<totalCost){
+    }else if(incomeAmmount<totalCost){
         alert("You can't spend more than you have!");
     }else{
         totalExpenses.innerText= totalCost;
-        balanceAfterExpenses.innerText=parseInt(incomeAmmount.value)-totalCost;
+        balanceAfterExpenses.innerText=incomeAmmount-totalCost;
     }
 }
-let savingPercentage = gettingElement('saving_field');
-let savingAmount = gettingElement('saving_ammount');
-let remainingBalance = gettingElement('remaining_balance');
-
-function savingCalculation(){
-    let updatedSavings =(parseInt(savingPercentage.value)/100)*parseInt(incomeAmmount.value);
-    let updatedRemainingBalance = parseInt(incomeAmmount.value)-(parseInt(totalExpenses.innerText)+updatedSavings);
-    if(updatedSavings>balanceAfterExpenses.innerText){
-        alert("you cant save more than you have!")
-    }else if(savingPercentage.value<0){
-        alert("'saving percentage' can't be negative")
+function calculatingSavings(){
+    var balanceAfterExpenses = getElement("balance");
+    var totalExpenses = getElement("total_expenses");
+    var incomeAmmount = getParsedValue("income_field");
+    var savingsPercentage = getParsedValue("saving_field"); 
+    var savingAmmout = getElement('saving_ammount');
+    var remainingBalance = getElement('remaining_balance');
+    var savings = (savingsPercentage/100)*incomeAmmount;
+    var finalRemainingBalance= incomeAmmount-(savings+ parseInt(totalExpenses.innerText));
+    if(savings>parseInt(balanceAfterExpenses.innerText)){
+        alert("you cant save more than you have!");
+    }else if(savingsPercentage<0 || isNaN(savingsPercentage) ){
+        alert("'saving percentage can't be negative or string!");
     }else{
-        savingAmount.innerText = updatedSavings;
-        remainingBalance.innerText=updatedRemainingBalance;
+        savingAmmout.innerText=savings;
+        remainingBalance.innerText=finalRemainingBalance;
     }
 }
 
+        
+
+
+
+// function savingCalculation(){
+//     var savingPercentage = getParsedValue('saving_field');
+//     var savingAmount = getElement('saving_ammount');
+//     var remainingBalance = getElement('remaining_balance');
+//     var updatedSavings =(savingPercentage/100)*incomeAmmount;
+//     var updatedRemainingBalance = incomeAmmount-(parseInt(totalExpenses.innerText)+updatedSavings);
+//     if(updatedSavings>balanceAfterExpenses.innerText){
+//         alert("you cant save more than you have!");
+//     }else if(savingPercentage.value<0){
+//         alert("'saving percentage' can't be negative");
+//     }else{
+//         savingAmount.innerText = updatedSavings;
+//         remainingBalance.innerText=updatedRemainingBalance;
+//     }
+// }   
